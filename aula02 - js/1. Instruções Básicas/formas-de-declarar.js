44371659a007ae3f1340fbdb398f4239899ec196
lucasmazaron <lucasmazaron@gmail.com>
2022-04-06 20:43:50 -0300

    Aula 2 - Variavel Booleana

---
+++ aula02 - js/1. Instruções Básicas/formas-de-declarar.js
@@ -0,0 +1,14 @@
+// METHOD 1
+var price = 5;
+var quantity = 14;
+var total = price * quantity;
+
+// METHOD 2
+var price, quantity, total;
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+// METHOD 3
+var price = 5, quantity = 14;
+var total = price * quantity;
---
+++ aula02 - js/1. Instruções Básicas/variavel-booleana/boolean.js
@@ -0,0 +1,14 @@
+var disponivel = true; //1 binario
+var liberadoParaVenda = false; //0 binario
+
+if(disponivel){
+    console.log('Em Estoque!');
+} else {
+    console.log('Fora de Estoque!');
+}
+
+if(liberadoParaVenda){
+    console.log('Liberado para venda!');
+} else {
+    console.log('Bloqueado para venda!');
+}
