fc908b4c3cdb95a5050ca349be7cf18b754f1479
lucasmazaron <lucasmazaron@gmail.com>
2022-04-06 20:53:53 -0300

    Aula 2 - Arrays

---
+++ aula02 - js/1. Instruções Básicas/array/array.js
@@ -0,0 +1,14 @@
+//indice      0      1           2        3
+//Posição     1      2           3        4
+var cores = ['azul', 'vermelho', 'verde', 'amarelo'];
+var numerosMegaSena = [10, 15, 25, 35, 12, 11];
+
+console.log('A primeira posição de cores é a cor: ' + cores[0]);
+console.log('A segunda posição de cores é a cor: ' + cores[1]);
+console.log('A terceira posição de cores é a cor: ' + cores[2]);
+console.log('A quarta posição de cores é a cor: ' + cores[3]);
+
+
+console.log(numerosMegaSena[1]);
+numerosMegaSena.sort();
+console.log(numerosMegaSena[1]);
